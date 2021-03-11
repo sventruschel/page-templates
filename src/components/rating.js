aba779619690b45a62c8ddc39b270ad924d41c3f
joerigroot <joeri.groot@bettyblocks.com>
2021-03-11 15:34:04 +0100

    Rating component

--- src/components/rating.js
+++ src/components/rating.js
@@ -4,40 +4,79 @@
   allowedTypes: [],
   orientation: 'VERTICAL',
   jsx: (() => {
+    const { Icons } = window.MaterialUI;
     const { Box } = window.MaterialUI.Core;
     const { Rating } = window.MaterialUI.Lab;
-    const { env } = B;
+    const { env, getCustomModelAttribute, useText } = B;
     const isDev = env === 'dev';
 
-    const [value, setValue] = React.useState(2);
+    const {
+      customModelAttribute: customModelAttributeObj,
+      size,
+      readonly,
+      icon,
+      iconSelected,
+    } = options;
 
-    return isDev ? (
+    const {
+      id: customModelAttributeId,
+      label = [],
+      value: defaultValue = [],
+    } = customModelAttributeObj;
+
+    const customModelAttribute = getCustomModelAttribute(
+      customModelAttributeId,
+    );
+
+    const [value, setValue] = useState(useText(defaultValue));
+
+    const IconEmptyComponent = React.createElement(Icons[icon], {
+      className: classes.root,
+    });
+
+    const IconSelectedComponent = React.createElement(Icons[iconSelected], {
+      className: classes.root,
+    });
+
+    const ratingBox = (
       <div>
         <Box component="fieldset" mb={3} borderColor="transparent">
-          <Rating name="read-only" value={value} readOnly />
-        </Box>
-      </div>
-    ) : (
-      <div>
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Rating name="read-only" value={value} readOnly />
+          <Rating
+            className={classes.root}
+            name="rating"
+            value={value}
+            defaultValue={customModelAttributeObj.value}
+            precision={1}
+            size={size}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            readOnly={readonly}
+            emptyIcon={IconEmptyComponent}
+            icon={IconSelectedComponent}
+          />
         </Box>
       </div>
     );
+
+    return isDev ? <div>{ratingBox}</div> : <div>{ratingBox}</div>;
   })(),
   styles: B => t => {
-    const { mediaMinWidth, Styling } = B;
+    const { Styling } = B;
     const style = new Styling(t);
-    const convertSizes = sizes =>
-      sizes.map(size => style.getSpacing(size)).join(' ');
-    const getSpacing = (idx, device = 'Mobile') =>
-      idx === '0' ? '0rem' : style.getSpacing(idx, device);
 
     return {
-      wrapper: {
-        display: 'inline-block',
+      root: {
+        '& .MuiRating-iconEmpty': {
+          color: ({ options: { emptyColor } }) => style.getColor(emptyColor),
+        },
+        '& .MuiRating-iconFilled': {
+          color: ({ options: { filledColor } }) => style.getColor(filledColor),
+        },
+        '& .MuiRating-iconHover': {
+          color: ({ options: { hoverColor } }) => style.getColor(hoverColor),
+        },
       },
-      root: {},
     };
   },
 }))();
