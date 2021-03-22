4b10af9574a12572cd89b16bae5196e186af879a
progbert <ro-ru@live.nl>
2021-03-22 22:10:27 +0100

    Renamed prefab

--- src/prefabs/pageWithCreateDataTable.js
+++ src/prefabs/pageWithCreateDataTable.js
@@ -1,5 +1,5 @@
 (() => ({
-  name: 'Page with dataTable and create dialog',
+  name: 'Page With Create Data Table',
   description: 'This is a page which contains a datatable with create dialog',
   icon: 'DataTable',
   category: 'DATA',
